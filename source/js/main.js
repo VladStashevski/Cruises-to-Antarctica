d79fa40ea42fd310c932c65198fbd63f22f63e49
Vladislav ST <adrenaline2142@gmail.com>
2022-07-27 15:12:45 +0500

    Update main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,23 @@
-const navigationMain = document.querySelector(".main-navigation");
-const navigationToggle = document.querySelector(".main-navigation__toggle");
-const showNavigation = document.querySelector(".main-navigation__wrapper");
-const mobileNavigation = document.querySelector(".main-navigation__mobile-menu");
-const menuItem = document.querySelectorAll(".main-navigation__wrapper a");
+const navigationMain = document.querySelector('.main-navigation');
+const navigationToggle = document.querySelector('.main-navigation__toggle');
+const showNavigation = document.querySelector('.main-navigation__wrapper');
+const mobileNavigation = document.querySelector('.main-navigation__mobile-menu');
+const menuItem = document.querySelectorAll('.main-navigation__wrapper a');
 
 function handlerToggleMenu(item) {
-  item.addEventListener("click", function () {
-    if (navigationMain.classList.contains("main-navigation--closed")) {
-      navigationMain.classList.remove("main-navigation--closed");
-      showNavigation.classList.add("main-navigation__wrapper--show");
-      navigationMain.classList.add("main-navigation--opened");
-      mobileNavigation.classList.add("main-navigation__mobile-menu-on");
+  item.addEventListener('click', function () {
+    if (navigationMain.classList.contains('main-navigation--closed')) {
+      navigationMain.classList.remove('main-navigation--closed');
+      showNavigation.classList.add('main-navigation__wrapper--show');
+      navigationMain.classList.add('main-navigation--opened');
+      mobileNavigation.classList.add('main-navigation__mobile-menu-on');
       disableScroll();
     } else {
-      navigationMain.classList.add("main-navigation--closed");
-      showNavigation.classList.remove("main-navigation__wrapper--show");
-      navigationMain.classList.remove("main-navigation--opened");
-      mobileNavigation.classList.remove("main-navigation__mobile-menu-on");
-      document.body.classList.remove("disable-scroll");
+      navigationMain.classList.add('main-navigation--closed');
+      showNavigation.classList.remove('main-navigation__wrapper--show');
+      navigationMain.classList.remove('main-navigation--opened');
+      mobileNavigation.classList.remove('main-navigation__mobile-menu-on');
+      document.body.classList.remove('disable-scroll');
       enableScroll();
     }
   });
@@ -27,74 +27,78 @@ handlerToggleMenu(navigationToggle);
 function closedMenu() {
   let widthViewport = window.innerWidth;
   if (widthViewport > 767) {
-    navigationMain.classList.add("main-navigation--closed");
-    showNavigation.classList.remove("main-navigation__wrapper--show");
-    navigationMain.classList.remove("main-navigation--opened");
-    mobileNavigation.classList.remove("main-navigation__mobile-menu-on");
+    navigationMain.classList.add('main-navigation--closed');
+    showNavigation.classList.remove('main-navigation__wrapper--show');
+    navigationMain.classList.remove('main-navigation--opened');
+    mobileNavigation.classList.remove('main-navigation__mobile-menu-on');
     enableScroll();
   }
 }
-window.addEventListener("resize", function () {
+window.addEventListener('resize', function () {
   closedMenu();
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (showNavigation.classList.contains("main-navigation__wrapper--show")) {
-      navigationMain.classList.add("main-navigation--closed");
-      showNavigation.classList.remove("main-navigation__wrapper--show");
-      navigationMain.classList.remove("main-navigation--opened");
-      mobileNavigation.classList.remove("main-navigation__mobile-menu-on");
+    if (showNavigation.classList.contains('main-navigation__wrapper--show')) {
+      navigationMain.classList.add('main-navigation--closed');
+      showNavigation.classList.remove('main-navigation__wrapper--show');
+      navigationMain.classList.remove('main-navigation--opened');
+      mobileNavigation.classList.remove('main-navigation__mobile-menu-on');
       enableScroll();
     }
   }
 });
 
-mobileNavigation.addEventListener("click", function () {
-  if (showNavigation.classList.contains("main-navigation__wrapper--show")) {
-    navigationMain.classList.add("main-navigation--closed");
-    showNavigation.classList.remove("main-navigation__wrapper--show");
-    navigationMain.classList.remove("main-navigation--opened");
-    mobileNavigation.classList.remove("main-navigation__mobile-menu-on");
+mobileNavigation.addEventListener('click', function () {
+  if (showNavigation.classList.contains('main-navigation__wrapper--show')) {
+    navigationMain.classList.add('main-navigation--closed');
+    showNavigation.classList.remove('main-navigation__wrapper--show');
+    navigationMain.classList.remove('main-navigation--opened');
+    mobileNavigation.classList.remove('main-navigation__mobile-menu-on');
     enableScroll();
   }
 });
 
 menuItem.forEach((item) => {
-  item.addEventListener("click", function () {
-    navigationMain.classList.add("main-navigation--closed");
-    navigationMain.classList.remove("main-navigation--opened");
-    mobileNavigation.classList.remove("main-navigation__mobile-menu-on");
+  item.addEventListener('click', function () {
+    navigationMain.classList.add('main-navigation--closed');
+    navigationMain.classList.remove('main-navigation--opened');
+    mobileNavigation.classList.remove('main-navigation__mobile-menu-on');
     enableScroll();
   });
 });
 
-navigationMain.classList.remove("main-navigation--nojs");
+navigationMain.classList.remove('main-navigation--nojs');
 
-const disableScroll = function () {document.body.classList.add('disable-scroll');}
+const disableScroll = function () {
+  document.body.classList.add('disable-scroll');
+};
 
-const enableScroll = function () {document.body.classList.remove('disable-scroll');}
+const enableScroll = function () {
+  document.body.classList.remove('disable-scroll');
+};
 
 // Маска ввода номера телефона
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener('DOMContentLoaded', function () {
   [].forEach.call(
-      document.querySelectorAll("#input-tel input"),
+      document.querySelectorAll('#input-tel input'),
       function (input) {
         let keyCode;
         function mask(event) {
           event.keyCode && (keyCode = event.keyCode);
           let pos = this.selectionStart;
           if (pos < 3) event.preventDefault();
-          let matrix = "8___ ___ ____",
+          let matrix = '8___ ___ ____',
             i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, ""),
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, ''),
             new_value = matrix.replace(/[_\d]/g, function (a) {
               return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
             });
-          i = new_value.indexOf("_");
+          i = new_value.indexOf('_');
           if (i != -1) {
             i < 5 && (i = 3);
             new_value = new_value.slice(0, i);
@@ -102,23 +106,23 @@ window.addEventListener("DOMContentLoaded", function () {
           let reg = matrix
               .substr(0, this.value.length)
               .replace(/_+/g, function (a) {
-                return "\\d{1," + a.length + "}";
+                return '\\d{1,' + a.length + '}';
               })
-              .replace(/[+()]/g, "\\$&");
-          reg = new RegExp("^" + reg + "$");
+              .replace(/[+()]/g, '\\$&');
+          reg = new RegExp('^' + reg + '$');
           if (
           !reg.test(this.value) ||
           this.value.length < 5 ||
           (keyCode > 47 && keyCode < 58)
           )
             this.value = new_value;
-          if (event.type == "blur" && this.value.length < 5) this.value = "";
+          if (event.type == 'blur' && this.value.length < 5) this.value = '';
         }
 
-        input.addEventListener("input", mask, false);
-        input.addEventListener("focus", mask, false);
-        input.addEventListener("blur", mask, false);
-        input.addEventListener("keydown", mask, false);
+        input.addEventListener('input', mask, false);
+        input.addEventListener('focus', mask, false);
+        input.addEventListener('blur', mask, false);
+        input.addEventListener('keydown', mask, false);
       }
   );
 });
